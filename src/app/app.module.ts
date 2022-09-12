eb03c20563fdd76d51e67c4590156da72aada3a0
Anshul Gupta <anshulgupta222@gmail.com>
2022-09-12 15:07:45 +0530

    Added New Component "Registration-Form"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationFormComponent
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/registration-form/registration-form.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SpaceValidators} from './space.validators';
+
+@Component({
+  selector: 'registration-form',
+  templateUrl: './registration-form.component.html',
+  styleUrls: ['./registration-form.component.css']
+})
+export class RegistrationFormComponent{
+
+  form = new FormGroup({
+    firstName : new FormControl('',[Validators.required,SpaceValidators.cannotContainSpace]),
+    lastName : new FormControl(),
+    email : new FormControl('',[Validators.required,Validators.email]),
+    password : new FormControl('',[Validators.required,Validators.pattern(/(?=.*[A-Z])/)])
+  });
+
+  get firstName()
+  {
+    return this.form.get('firstName');
+  }
+
+  get email()
+  {
+    return this.form.get('email');
+  }
+
+  get password()
+  {
+    return this.form.get('password');
+  }
+  register(){
+    this.form.setErrors({
+      inValidRegistration:true,
+    })
+  }
+}
