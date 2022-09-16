26035f6a0a00a2c24c51442cc56d65559dc5ddc3
Anshul Gupta <anshulgupta222@gmail.com>
2022-09-16 17:02:03 +0530

    Adding Login-Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationFormComponent
+    RegistrationFormComponent,
+    LoginFormComponent
   
   ],
   imports: [
